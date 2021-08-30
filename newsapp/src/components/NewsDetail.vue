a44490c0167f4c8d79b974bb0a9a57c020a18283
vineeth <vnthvj@gmail.com>
2021-08-31 03:22:25 +0700

    Moving files from local project to git project

---
+++ newsapp/src/components/NewsDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            news: {
+                title: '',
+                content: ''
+            }
+        }
+    },
+    moumted(){
+        this.fetchNews(this.$route.params.id);
+    },
+    watch: {
+        "$router.params.id": {
+            handler(){
+                this.fetchNews(this.$route.params.id);
+            },
+            imediate:true
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- newsapp/src/router/index.js
+++ newsapp/src/router/index.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import NewsDetail from '../components/NewsDetail.vue';
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/news/:id',
+    name: 'News',
+    component: NewsDetail
   },
   {
     path: '/about',
